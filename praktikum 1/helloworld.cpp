80900ccf003b19c7524dd5cceb4a33506fa12eb6
Laviettan <nadhiraa488@gmail.com>
2026-09-13 19:07:54 +0700

    Add files via upload

---
+++ praktikum 1/helloworld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string nama_bunga[3] = {
+        "poppy", "matahari", "dandelion"
+    };
+
+    for (int i = 0; i < 3; i++) {
+        cout << nama_bunga[i] << endl;
+    }
+    cout << "Hello World";
+    
+    return 0;
+}
+
+
+
